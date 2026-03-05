5fbe8867d01f33c6c0934693114dfbaa6ea7cb62
christiannshuti <chrisnshuti943@gmail.com>
2026-03-05 08:32:57 +0200

    Filled the Course entity

---
+++ src/main/java/entity/Course.java
@@ -0,0 +1,44 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String instructor;
+
+    public Course() {}
+
+    public Course(String title, String instructor) {
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+}
