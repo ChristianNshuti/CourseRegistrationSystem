1e883ce3238f827d5868f9e24116723653409cb9
christiannshuti <chrisnshuti943@gmail.com>
2026-03-05 08:39:06 +0200

    Filled the registration entity

---
+++ src/main/java/entity/Registration.java
@@ -0,0 +1,47 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "registrations")
+public class Registration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public Registration() {}
+
+    public Registration(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
