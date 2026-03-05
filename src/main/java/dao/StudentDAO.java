d1d57ca437789db6b67ebf217b4a6916252f1649
christiannshuti <chrisnshuti943@gmail.com>
2026-03-05 15:18:52 +0200

    Defined StudentDAO

---
+++ src/main/java/dao/StudentDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+import java.util.*;
+
+public class StudentDAO {
+
+}
