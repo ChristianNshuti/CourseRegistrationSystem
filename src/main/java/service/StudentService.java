6dc465c2edf057ebd021ff47a5b2f4bd7ce5d600
christiannshuti <chrisnshuti943@gmail.com>
2026-03-05 15:58:53 +0200

    Made studentService

---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,32 @@
+package service;
+
+import dao.StudentDAO;
+import entity.Student;
+
+import java.util.*;
+
+public class StudentService {
+
+    private final StudentDAO studentDAO = new StudentDAO();
+
+    public void saveStudent(String name,String email) {
+        studentDAO.saveStudent(name,email);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentDAO.getAllStudents();
+    }
+
+    public Student getStudentById(Long studentId) {
+        return studentDAO.getStudentById(studentId);
+    }
+
+    public void updateStudent(Long studentId,String name,String email) {
+        studentDAO.updateStudent(studentId,name,email);
+    }
+
+    public void deleteStudent(Long studentId) {
+        studentDAO.deleteStudent(studentId);
+    }
+
+}
